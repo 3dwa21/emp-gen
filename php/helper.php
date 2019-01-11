45fd860aaf4e1facbcc4ea4e9dd9666d31475955
3dwa21 <walen@gmx.net>
2019-01-11 17:03:03 +0100

    integrated bootstrap (offline) in mod_settings integrated fontawsome for login/logout symbol in nav-bar

--- php/helper.php
+++ php/helper.php
@@ -33,10 +33,47 @@
 
         include($path."php/globals.php"); 
         if(isset($_COOKIE[$cookiename])) {
-            return "<li><a href=\"".$path."php/logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> Logout</a></li>";
+            return "<li><a class=\"nav-link\" href=\"".$path."php/logout.php\"><i class=\"fas fa-sign-out-alt\"></i> Logout</a></li>";
         } else {
-            return "<li><a href=\"login.html\"><span class=\"glyphicon glyphicon-log-in\"></span> Login</a></li>";
+            return "<li><a class=\"nav-link\" href=\"login.html\"><i class=\"fas fa-sign-in-alt\"></i> Login</a></li>";
         }
+    }
+
+    //===========================================================================//
+
+    function loadMods() {
+        include("globals.php");
+
+        $output = "";
+								
+        // get and display data from database
+        $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $dbusername, $dbpassword);
+        $sql = "SELECT * FROM mods";
+        
+        $output = $output."<table>";
+        
+        $numofmods = 0;
+        foreach ($pdo->query($sql) as $row) {
+        
+            // check if current mod is set active
+            $checked = "";
+            if ($row["2_active"] == 1) {
+                $checked = " checked";
+            }
+            $modname = $row["1_name"];
+            $modid = $row["0_ID"];
+            
+            $output = $output."<tr><td><input type=\"hidden\" name=\"".$modid."\" value=\"0\">";
+            $output = $output."<input type=\"checkbox\" name=\"".$modid."\" value=\"1\"".$checked."> ".$modname."</td></tr>";
+        
+            $numofmods++;
+        }
+        
+        $output = $output."</table><br><br>";
+        $output = $output."<input type=\"hidden\" name=\"modcounter\" value=\"".$numofmods."\">";
+        $output = $output."<button type=\"submit\" class=\"btn btn-success btn-lg\">Save</button";
+
+        return $output;
     }
 
     //===========================================================================//
